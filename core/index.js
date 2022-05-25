644aba63ebf0cd4e7bf83e2d8918b0fdafba09b1
Agustin Laurito <agustin.laurito@coltech.com.ar>
2022-05-24 23:13:26 -0300

    update package.json

--- core/index.js
+++ core/index.js
@@ -2,6 +2,16 @@
 
 const app = require('../app');
 const config = require('../config/default');
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+
+const url = "http://www.tams.com.ar/organismos/vuelos.aspx";
+axios(url).then((response) => {
+  const html_data = response.data;
+  const $ = cheerio.load(html_data);
+  console.log(response);
+});
 
 
 app.get('/', (req, res) => {
